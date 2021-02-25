65b9c2874f32b0283d819607c80723e36f7e6a69
Mahdi Salmanzadeh <salmanzadeh@payampardaz.com>
2021-02-25 13:08:08 +0330

    move mscrypt codes to a seperated class

--- CryptoProject/MSCrypt.h
+++ CryptoProject/MSCrypt.h
@@ -1,7 +1,20 @@
 #pragma once
+#include <string>
+#include <memory>
+#include "RoundKeyGenerator.h"
 
 class MSCrypt
 {
-	
+	std::string key_;
+	std::unique_ptr<RoundKeyGenerator> rkGen_;
+
+	uint8_t sbox(uint8_t in);
+	uint32_t pbox(uint32_t in);
+	uint32_t F(uint32_t in, uint32_t roundKey);
+
+public:
+	MSCrypt(std::string key);
+	std::string enc(std::string plain);
+	std::string dec(std::string cipher);
 };
 
